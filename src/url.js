12a74e84793796255785805386f85d0764b311ca
Rahul Sharma <rahulpancholi3937@gmail.com>
2023-05-20 16:56:13 +0530

    fix

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const Global_Url = "http://localhost:8000"
-// export const Global_Url = "https://fakegramapi.onrender.com"
+// export const Global_Url = "http://localhost:8000"
+export const Global_Url = "https://fakegramapi.onrender.com"
